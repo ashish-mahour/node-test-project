6070ae974aac321420a7bc8b42b29eb215a0bdb7
Ashish Kumar <40929533+ashish-mahour@users.noreply.github.com>
2025-02-07 16:17:06 +0530

    Push Notification | Android | Sensitive Information Removed

--- android-push.js
+++ android-push.js
@@ -1,19 +1,19 @@
 const FCM = require('fcm-node')
 
 // var serverKey = require('./fcm.json') //put the generated private key path here    
-var serverKey = 'AAAAdlmIxCg:APA91bFPCRgJQ6uxKHntAjFqCrK2OVtp9une-3FzGEVAy2-T__VUVnxtp1wJIoJeOyv4krsFZbMWcZUJBoGR9dPj4YLLQKBnaVVvnj4oUFKHMoFRnY01DY9Z7CUAmFC6kuvITz_qgD9x'
+var serverKey = '' // Add Server Key Here
 
 var fcm = new FCM(serverKey)
-console.log(JSON.stringify(fcm))
+// console.log(JSON.stringify(fcm))
 
 var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-    to: 'cYOUSQDTmUNIjBcN65NBdZ:APA91bHnfxProG0zOQSohVVUUe9XXFDKOQGC3qqa4cw8o_Mkw0Qq5Amjbw8u5tZjaTE6PLGKwnTclWyXDAMIGZwcStAodbex_E9GZ8_0jk5bBU8fEDuKAyQ',
+    to: '', // Add Token Here
     // collapse_key: 'Updates Available',
 
     notification: {
-        body: 'Reservation confirmed At K31! ',
-        image: "https://prod-tlc.my.salesforce-sites.com/clubmarriott/resource/1611744960000/TLCLogo",
-        title: 'Reservation at k3',
+        body: 'Test',
+        image: "<image>",
+        title: 'Test',
         foreground: true,
         content_available: true,
         mutable_content: true
@@ -22,12 +22,7 @@ var message = { //this may vary according to the message type (single recipient,
         notification: {
             imageUrl: 'https://foo.bar.pizza-monster.png'
         }
-    },
-    data: {
-        obj: 1, obh1: 2,
-        forceShow: true
     }
-
 }
 fcm.send(message, (err, response) => {
     if (err) {
