b2731109e72e57bafa70fa6d857b41780ff3becc
Ashish Kumar <40929533+ashish-mahour@users.noreply.github.com>
2025-02-07 16:18:48 +0530

    Push Notification | iOS | Sensitive Information Removed

--- ios-push.js
+++ ios-push.js
@@ -5,9 +5,9 @@ if (process.argv?.length === 0)  {
 const apn = require('node-apn');
 var apnProvider = new apn.Provider({
 token: {
-    key: './AuthKey_F7LD5B38C3.p8', // optionally: fs.readFileSync('./certs/key.p8')
-    keyId: 'F7LD5B38C3',
-    teamId: 'VKVQ7DZ2QT',
+    key: '', // optionally: fs.readFileSync('./certs/key.p8') // P8 File
+    keyId: '', // Key ID
+    teamId: '', // Team ID
 },
 production: false
 });
@@ -16,13 +16,12 @@ note.rawPayload = {
     aps: {
         alert: {
             title: "Hello CM User!!",
-            body: "Welcome to Club Marriott!!"       
+            body: "Welcome!!"       
         },
         badge: 0,
         "mutable-content": 1,
         "content-available": 1,
         sound: "default",
-        category: "br.com.inngage.Custom-Notification-Interface.notification",
         provider: "inngage",
         imageURL: "https://cdn.i-scmp.com/sites/default/files/d8/images/methode/2020/08/28/fda91d6c-e6b0-11ea-8600-abe4f45458c9_image_hires_171751.png"
     },
@@ -34,7 +33,7 @@ note.rawPayload = {
     // },
     "media-attachment-url": "https://www.cvent.com/sites/default/files/image/2023-02/five%20star%20hotel%20lobby.jpg"
 };
-note.topic = "com.tlcgroup.customer.clubmarriott"
+note.topic = "" // App ID
 let deviceToken = process.argv[process.argv.length - 1]
 apnProvider.send(note, deviceToken).then( (result) => {
     // see documentation for an explanation of result
